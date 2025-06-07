6f9c490ae5fd25b59278fdb7bdb6f8de8f7876b7
Braulio <braulio.diez@gmail.com>
2025-06-07 20:16:44 +0200

    simple demo

---
+++ src/pages/training/training.model.ts
@@ -0,0 +1,7 @@
+export interface Lesson {
+  id: string;
+  title: string;
+  slug: string;
+  video: string; // url del video de YouTube
+  content: string; // descripción o contenido de la lección, se muestra abajo
+}
